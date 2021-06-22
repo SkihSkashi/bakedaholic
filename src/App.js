7340bae9317490d1850c519ff752fccc474d4e26
Pierre-at-199487244040 <9BpqGfKbx6NVtdCyAgL7XMdnx3+vlgCa5caZmxc44Lo=>
2021-06-22 19:50:57 +0200

    updated favicon

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Amplify from 'aws-amplify';
 import awsconfig from './aws-exports';
-import { AmplifySignOut, withAuthenticator } from '@aws-amplify/ui-react';
+// import { AmplifySignOut, withAuthenticator } from '@aws-amplify/ui-react';
 Amplify.configure(awsconfig);
 
 function App() {
@@ -17,4 +17,4 @@ function App() {
     </div>
   );
 }
-export default withAuthenticator(App);
+export default App;
