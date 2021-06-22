89bf7660e4abd75879548892595cb3bd35b8f69a
Pierre-at-199487244040 <9BpqGfKbx6NVtdCyAgL7XMdnx3+vlgCa5caZmxc44Lo=>
2021-06-22 21:45:34 +0200

    Removed remember credit card details - which is not needed

--- src/PaymentForm.js
+++ src/PaymentForm.js
@@ -46,12 +46,6 @@ export default function PaymentForm() {
             autoComplete="cc-number"
           />
         </Grid>
-        <Grid item xs={12}>
-          <FormControlLabel
-            control={<Checkbox color="secondary" name="saveCard" value="yes" />}
-            label="Remember credit card details for next time"
-          />
-        </Grid>
       </Grid>
     </React.Fragment>
   );
