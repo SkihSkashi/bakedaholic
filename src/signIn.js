4d59eda59b81f4cfe956e9062b355077f70bcdbb
Pierre-at-199487244040 <9BpqGfKbx6NVtdCyAgL7XMdnx3+vlgCa5caZmxc44Lo=>
2021-07-24 10:59:21 +0200

    testAuth

--- src/signIn.js
+++ src/signIn.js
@@ -11,7 +11,6 @@ import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
-import { AmplifySignOut, withAuthenticator } from '@aws-amplify/ui-react';
 import Container from '@material-ui/core/Container';
 
 function Copyright() {
@@ -51,63 +50,72 @@ export default function SignIn() {
   const classes = useStyles();
 
   return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign in
-        </Typography>
-        <form className={classes.form} noValidate>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
-            autoFocus
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-          />
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit} href="/Checkout"
-          >
-            Sign In
-          </Button>
-          <Grid container>
-            <Grid item xs>
-              <Link href="https://www.youtube.com/" variant="body2">
-                Forgot password?
-              </Link>
-            </Grid>
-          </Grid>
-        </form>
-      </div>
-      <Box mt={8}>
-        <Copyright />
-      </Box>
-    </Container>
+      <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          color="primary"
+          className={classes.submit} href="/Checkout"
+      >
+        Sign In
+      </Button>
+    // <Container component="main" maxWidth="xs">
+    //   <CssBaseline />
+    //   <div className={classes.paper}>
+    //     <Avatar className={classes.avatar}>
+    //       <LockOutlinedIcon />
+    //     </Avatar>
+    //     <Typography component="h1" variant="h5">
+    //       Sign in
+    //     </Typography>
+    //     <form className={classes.form} noValidate>
+    //       <TextField
+    //         variant="outlined"
+    //         margin="normal"
+    //         required
+    //         fullWidth
+    //         id="email"
+    //         label="Email Address"
+    //         name="email"
+    //         autoComplete="email"
+    //         autoFocus
+    //       />
+    //       <TextField
+    //         variant="outlined"
+    //         margin="normal"
+    //         required
+    //         fullWidth
+    //         name="password"
+    //         label="Password"
+    //         type="password"
+    //         id="password"
+    //         autoComplete="current-password"
+    //       />
+    //       <FormControlLabel
+    //         control={<Checkbox value="remember" color="primary" />}
+    //         label="Remember me"
+    //       />
+    //       <Button
+    //         type="submit"
+    //         fullWidth
+    //         variant="contained"
+    //         color="primary"
+    //         className={classes.submit} href="/Checkout"
+    //       >
+    //         Sign In
+    //       </Button>
+    //       <Grid container>
+    //         <Grid item xs>
+    //           <Link href="https://www.youtube.com/" variant="body2">
+    //             Forgot password?
+    //           </Link>
+    //         </Grid>
+    //       </Grid>
+    //     </form>
+    //   </div>
+    //   <Box mt={8}>
+    //     <Copyright />
+    //   </Box>
+    // </Container>
   );
 }
